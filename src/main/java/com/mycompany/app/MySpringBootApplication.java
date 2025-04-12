62d0fc518511925cf35da151a888665900013882
Nick <nicolasgomezaragon@proton.me>
2025-04-11 19:09:04 -0500

    Adjusted structure for project with MVN

---
+++ src/main/java/com/mycompany/app/MySpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+    @RestController
+    class MyController {
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello, Spring Boot!";
+        }
+    }
+}
